b0cf78882be98b120b5fcfe88cf7fc921f332ec1
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-01-05 15:58:40 -0800

    generate HTML file

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const printProfileData = (profileDataArr) => {
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
